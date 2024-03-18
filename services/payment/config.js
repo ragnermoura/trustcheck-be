7d441d708c9e17ee4b12f5ede5e639da9e22a238
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2024-03-18 23:07:56 +0000

    Ajuste

--- services/payment/config.js
+++ services/payment/config.js
@@ -6,5 +6,5 @@ module.exports = {
     sandbox: true,
     client_id: process.env.CLIENT_ID_HOMOLOG,
     client_secret: process.env.CLIENT_SECRET_HOMOLOG,
-	pix_cert: __dirname + './homologacao-548331-trustPlataformHomolog.p12',
+	pix_cert: __dirname + '/homologacao-548331-trustPlataformHomolog.p12',
 }
